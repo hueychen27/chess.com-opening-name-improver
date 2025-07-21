500f4ffecc94ecc0efa8f575db4f4c86b6c5e5f3
Da_Person <hueychen27@outlook.com>
2025-07-21 16:59:04 -0700

    Make opening name selectable for daily games

--- extend_opening_name.js
+++ extend_opening_name.js
@@ -8,14 +8,14 @@ function onErr(err) {
 /**
  * 
  * @param {string} selector CSS-style selector
- * @returns {Promise<Element | null>}
+ * @returns {Promise<Element>}
  */
 const querySelector = async (selector) => await waitForExistenceOf(selector);
 
 /**
  * 
  * @param {string} selector CSS-style selector
- * @returns {Promise<Element | null>}
+ * @returns {Promise<Element>}
  */
 const waitForExistenceOf = (selector) => new Promise(resolve => {
     if (document.querySelector(selector)) return resolve(document.querySelector(selector));
@@ -46,6 +46,7 @@ async function extendOpeningName() {
     const extendedOpeningNameText = /\d/.test(openingLinkEl.href) ? openingLinkEl.href.replace(/^\D*(?=\d)/, "").replace(/(?<!O-)O-O-(?!O)/g, " O-O ").replace(/(?<!O)-(?!O)/g, " ").replace(/(\d)/, ": $1").replace(/O-O-O-O/g, "O-O O-O") : "";
     span.innerHTML = extendedOpeningNameText;
     span.className = "eco-opening-name-extended";
+    span.style.userSelect = "text";
     /* Failed attempt at reconstructing the opening name purely from the link
     //@ if (el.hasAttribute("href")) document.querySelector(".eco-opening-name").innerHTML = el.href.replace("https://www.chess.com/openings/", "").replace(/-(?!(Indian|Kann|Slav))/g, " ").replace(/(?<=Opening)\s|(?<=(?<!Petrovs Defense: Three Knights) Game)\s|(?<=Attack)\s|(?<=Queens Gambit (Accepted|Declined))\s|(?<=Kings Gambit (Accepted|Declined))\s|(?<=(French|Sicilian|Petrovs|Scandinavian|Alekhines|Caro-Kann|Dutch|Pirc|Old Benoni|Alapin Sicilian|Closed Sicilian|Slav|Kings Indian|Philidor|Old Indian|Bogo-Indian|Queens Indian|Nimzo-Indian|Grunfeld|Benoni|Semi-Slav) Defense)\s/, ": ").replace(/(?<=Variation)\s|(?<=Sacrifice(?! Variation))\s/, ", ").replace(/(?<=(King|Queen|Alekhine|Petrov))s/g, "'s").replace(/ (?=[\d])/, ": ").replace(/::|,:/g, ":").replace("Grunfeld", "Grünfeld");
     */
@@ -58,8 +59,18 @@ console.log("Opening extender working...");
 async function main() {
     if (await browser.storage.local.get("toggle").then(item => item.toggle, onErr) ?? true) {
         await extendOpeningName();
+
+        /**
+         * @type {HTMLSpanElement}
+         */
         const openingNameEl = await querySelector(".eco-opening-name");
+        // Want to make it selectable always
+        openingNameEl.style.userSelect = "text";
+
         await browser.storage.local.get("overflowBehavior").then(async item => {
+            /**
+             * @type {HTMLDivElement}
+             */
             const openingNameContainerEl = await querySelector(isDaily() ? ".eco-opening-component" : ".eco-opening-panel");
             openingNameEl.style.overflow = openingNameContainerEl.style.overflow = item.overflowBehavior == "unset" ? "unset" : "";
             openingNameEl.style.overflowX = openingNameContainerEl.style.overflowX = item.overflowBehavior == "unset" ? "scroll" : "";
