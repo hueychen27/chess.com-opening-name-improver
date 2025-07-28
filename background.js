2f4e625861ab3012a6b59efe4cd9b9c4e48e8996
Da_Person <--help>
2025-07-27 21:08:59 -0700

    Add support (background script) for synchronizing synced settings on browser installation and fix bug where retrieving synced settings does not sync with local settings

---
+++ background.js
@@ -0,0 +1,11 @@
+import { getAndSyncSynchronizedSettings, SyncError } from "./common/getSyncState.js"
+
+browser.runtime.onInstalled.addEventListener(async () => {
+    const settings = await getAndSyncSynchronizedSettings();
+    if (settings instanceof SyncError) {
+        console.error("A SyncError occured:", settings);
+        return false;
+    }
+    console.log("Synced settings retrieved:", settings);
+    console.log("Successfully synced settings.");
+})
--- popup/popup.js
+++ popup/popup.js
@@ -17,143 +17,152 @@ function onErr(err) {
     log(err);
 }
 
-(async () => {
-    /**
-     * 
-     * @param {string} elementId id name
-     * @returns {Promise<Element | null>}
-     */
-    const getElementById = async (elementId) => await waitForExistenceOfId(elementId);
+/**
+ * 
+ * @param {string} elementId id name
+ * @returns {Promise<Element | null>}
+ */
+const getElementById = async (elementId) => await waitForExistenceOfId(elementId);
 
-    /**
-     * 
-     * @param {string} elementId id name
-     * @returns {Promise<Element | null>}
-     */
-    const waitForExistenceOfId = (elementId) => new Promise(resolve => {
-        if (document.getElementById(elementId)) return resolve(document.getElementById(elementId));
-        const observer = new MutationObserver(() => {
-            if (document.getElementById(elementId)) {
-                observer.disconnect();
-                resolve(document.getElementById(elementId));
-            }
-        });
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    }) // https://stackoverflow.com/a/61511955/15055490
-
-    /**
-     * @type {HTMLSelectElement}
-    */
-    const overflowBehaviorEl = document.getElementById("overflowBehavior");
-    /**
-     * @type {HTMLButtonElement}
-     */
-    const toggleBtn = document.getElementById("toggle");
-
-    function setToggleState(state = true) {
-        toggle.innerHTML = (state ?? true) ? "Disable Extension" : "Enable Extension";
-        toggle.setAttribute("data-state", state ?? true);
-    }
-
-    function setOverflowBehavior(behavior = "unset") {
-        overflowBehaviorEl.selectedIndex = behavior === "ellipses" ? 0 : 1;
-    }
-
-    async function getSynchronizedSettings() {
-        await browser.storage.sync.get().then(item => {
-            // log("item: " + JSON.stringify(item));
-            if (Object.keys(item).length !== 2) return;
-            setToggleState(item.toggle);
-            setOverflowBehavior(item.overflowBehavior);
-        }, onErr)
-    }
-
-    async function deleteSynchronizedSettings() {
-        await browser.storage.sync.clear().then(() => { }, onErr)
-    }
-
-    async function synchronizeSettings() {
-        let toggle = null;
-        await browser.storage.local.get("toggle").then(async (item) => toggle = item.toggle, onErr);
-        browser.storage.sync.set({
-            toggle: toggle ?? true,
-            overflowBehavior: overflowBehaviorEl.value
-        })
-    }
-
-    await browser.storage.local.get("toggle").then(async (item) => {
-        setToggleState(item.toggle);
-        item.toggle ?? await browser.storage.local.set({ toggle: true });
-    }, onErr)
-
-    await browser.storage.local.get("overflowBehavior").then((item) => {
-        setOverflowBehavior(item.overflowBehavior);
-    }, onErr)
-
-    toggleBtn.addEventListener("click", () => {
-        toggle.setAttribute("data-state", !(toggle.getAttribute("data-state") === "true"));
-        toggle.innerHTML = toggle.getAttribute("data-state") == 'true' ? "Disable Extension" : "Enable Extension";
-        browser.storage.local.set({ toggle: toggle.getAttribute("data-state") === "true" });
-    })
-
-    overflowBehaviorEl.addEventListener("change", () => {
-        browser.storage.local.set({ overflowBehavior: overflowBehaviorEl.value });
-    })
-
-    document.getElementById("syncButton").addEventListener("click", synchronizeSettings);
-
-    document.getElementById("retrieveSyncButton").addEventListener("click", getSynchronizedSettings);
-
-    /**
-     * id is setTimeout id and time is the start time in seconds
-     * @type {{id: number, time: number}[]}
-     */
-    let timeoutId = [];
-    document.getElementById("deleteSyncButton").addEventListener("click", async function () {
-        if (timeoutId.length !== 0) {
-            timeoutId.forEach(({ id: val }) => clearTimeout(val));
-            timeoutId = [];
+/**
+ * 
+ * @param {string} elementId id name
+ * @returns {Promise<Element | null>}
+ */
+const waitForExistenceOfId = (elementId) => new Promise(resolve => {
+    if (document.getElementById(elementId)) return resolve(document.getElementById(elementId));
+    const observer = new MutationObserver(() => {
+        if (document.getElementById(elementId)) {
+            observer.disconnect();
+            resolve(document.getElementById(elementId));
         }
-        this.innerHTML = "Delete synced settings";
-        if (this.getAttribute("data-really") === "false") {
-            this.innerHTML = "";
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+}) // https://stackoverflow.com/a/61511955/15055490
 
-            const revertCounter = document.createElement("span");
-            revertCounter.id = "revertCounter";
-            revertCounter.textContent = "5";
+/**
+ * @type {HTMLSelectElement}
+*/
+const overflowBehaviorEl = document.getElementById("overflowBehavior");
+/**
+ * @type {HTMLButtonElement}
+ */
+const toggleBtn = document.getElementById("toggle");
 
-            const abbr = document.createElement("abbr");
-            abbr.title = "seconds";
-            abbr.textContent = "sec";
+async function overrideLocalSettings({ toggle, overflowBehavior }) {
+    await browser.storage.local.clear();
+    await browser.storage.local.set({ toggle, overflowBehavior });
+}
 
-            this.append("Really delete? (", revertCounter, " ", abbr, " to cancel)");
+function setToggleState(state = true) {
+    toggle.innerHTML = (state ?? true) ? "Disable Extension" : "Enable Extension";
+    toggle.setAttribute("data-state", state ?? true);
+}
 
-            this.setAttribute("data-really", "true");
-            timeoutId.push({
-                id: setTimeout(() => {
-                    this.setAttribute("data-really", "false");
-                    this.textContent = "Delete synced settings"
-                }, 5000), time: Date.now() / 1000
-            })
+function setOverflowBehavior(behavior = "unset") {
+    overflowBehaviorEl.selectedIndex = behavior === "ellipses" ? 0 : 1;
+}
+
+async function getSynchronizedSettings() {
+    await browser.storage.sync.get().then(async item => {
+        // log("item: " + JSON.stringify(item));
+        if (Object.keys(item).length !== 2) {
+            if (Object.keys(item).length === 0) log("Synced settings is empty.")
             return;
         }
-        this.setAttribute("data-really", "false");
-        await deleteSynchronizedSettings();
-    })
+        setToggleState(item.toggle);
+        setOverflowBehavior(item.overflowBehavior);
+        await overrideLocalSettings(item);
+    }, onErr)
+}
 
-    async function updateRevertCounter() {
-        /**
-         * @type {HTMLSpanElement | null}
-         */
-        const revertCounter = await getElementById("revertCounter");
-        if (!(revertCounter === null || timeoutId.length === 0)) {
-            const durationLeft = (Math.round((5 - (((Date.now() / 1000) - timeoutId[timeoutId.length - 1].time))) * 10) / 10).toFixed(1);
-            revertCounter.textContent = durationLeft;
-        }
-        window.requestAnimationFrame(updateRevertCounter);
+
+async function deleteSynchronizedSettings() {
+    await browser.storage.sync.clear().then(() => { }, onErr);
+}
+
+async function synchronizeSettings() {
+    let toggle = null;
+    await browser.storage.local.get("toggle").then(async (item) => toggle = item.toggle, onErr);
+    browser.storage.sync.set({
+        toggle: toggle ?? true,
+        overflowBehavior: overflowBehaviorEl.value
+    })
+}
+
+await browser.storage.local.get("toggle").then(async (item) => {
+    setToggleState(item.toggle);
+    item.toggle ?? await browser.storage.local.set({ toggle: true });
+}, onErr)
+
+await browser.storage.local.get("overflowBehavior").then((item) => {
+    setOverflowBehavior(item.overflowBehavior);
+}, onErr)
+
+toggleBtn.addEventListener("click", () => {
+    toggle.setAttribute("data-state", !(toggle.getAttribute("data-state") === "true"));
+    toggle.innerHTML = toggle.getAttribute("data-state") == 'true' ? "Disable Extension" : "Enable Extension";
+    browser.storage.local.set({ toggle: toggle.getAttribute("data-state") === "true" });
+})
+
+overflowBehaviorEl.addEventListener("change", () => {
+    browser.storage.local.set({ overflowBehavior: overflowBehaviorEl.value });
+})
+
+document.getElementById("syncButton").addEventListener("click", synchronizeSettings);
+
+document.getElementById("retrieveSyncButton").addEventListener("click", getSynchronizedSettings);
+
+/**
+ * id is setTimeout id and time is the start time in seconds
+ * @type {{id: number, time: number}[]}
+ */
+let timeoutId = [];
+document.getElementById("deleteSyncButton").addEventListener("click", async function () {
+    if (timeoutId.length !== 0) {
+        timeoutId.forEach(({ id: val }) => clearTimeout(val));
+        timeoutId = [];
+    }
+    this.innerHTML = "Delete synced settings";
+    if (this.getAttribute("data-really") === "false") {
+        this.innerHTML = "";
+
+        const revertCounter = document.createElement("span");
+        revertCounter.id = "revertCounter";
+        revertCounter.textContent = "5";
+
+        const abbr = document.createElement("abbr");
+        abbr.title = "seconds";
+        abbr.textContent = "sec";
+
+        this.append("Really delete? (", revertCounter, " ", abbr, " to cancel)");
+
+        this.setAttribute("data-really", "true");
+        timeoutId.push({
+            id: setTimeout(() => {
+                this.setAttribute("data-really", "false");
+                this.textContent = "Delete synced settings"
+            }, 5000), time: Date.now() / 1000
+        })
+        return;
+    }
+    this.setAttribute("data-really", "false");
+    await deleteSynchronizedSettings();
+    log("Deleted sync settings");
+})
+
+async function updateRevertCounter() {
+    /**
+     * @type {HTMLSpanElement | null}
+     */
+    const revertCounter = await getElementById("revertCounter");
+    if (!(revertCounter === null || timeoutId.length === 0)) {
+        const durationLeft = (Math.round((5 - (((Date.now() / 1000) - timeoutId[timeoutId.length - 1].time))) * 10) / 10).toFixed(1);
+        revertCounter.textContent = durationLeft;
     }
     window.requestAnimationFrame(updateRevertCounter);
-})()
+}
+window.requestAnimationFrame(updateRevertCounter);
